718a9c3251cfe411cdaa03f9351406c7b5f3d78c
Pedro Henrique <pholiveirabrito@hotmail.com>
2022-01-29 10:38:01 -0300

    Atualização Repositório - Novas Pastas e Anotações Inseridas

---
+++ Primeiros Passos com .Net/Iniciando .Net/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DigitalInnovationOne{
+    class Program {
+        static void Main(string[] args) {
+            for(int i = 1; i < 6; i++) {
+                Console.WriteLine($"Digite o {i}º número: ");
+            }
+        }
+    }
+}
